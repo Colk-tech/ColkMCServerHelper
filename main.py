4153ba65443548cf8ed5705f071fd33cfff4242d
Colk-tech <mk239312@icloud.com>
2019-10-24 18:24:26 +0900

    Complete covering cmd into Python at issue #1

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ import sys
 import time
 import shutil
 ut = time.time()
-while_flag = True
-error_flag = False
 def flag_initialize():
     global while_flag
     global error_flag
     while_flag = True
     error_flag = False
 flag_initialize()
+Current_Directory: str = os.getcwd()
 # === end initialize === #
 
 # === ask user if want to backup === #
@@ -40,13 +39,13 @@ flag_initialize()
 if bool(backup_flag) == True:
     backup_error_flag = False
     try:
-        shutil.copytree(os.getcwd(),"../Backup"+"/"+str(ut)+"/")
+        shutil.copytree(os.getcwd(), "../Backup" + "/" + str(ut) + "/")
     except:
-        backup_erroe_flag = True
+        backup_error_flag = True
     if bool(backup_error_flag) == False:
         print("バックアップが完了しました。")
         print("サーバーを起動します...")
-    else:
+    elif bool(backup_error_flag) == True:
         while bool(while_flag):
             print("バックアップに失敗しました。")
             print("サーバーを起動しますか？")
@@ -64,4 +63,45 @@ if bool(backup_flag) == True:
                 else:
                     print("Y/N以外が入力されました。")
                     print()
-    # === end "backup data" === #
+# === end "backup data" === #
+
+flag_initialize()
+
+# === BOOT SERVER === #
+os.system("cd"+" "+'"'+Current_Directory+'"')
+os.system("java -Xms4096M -Xmx4096M -jar" + " server.jar " + "-o true")
+# === end "BOOT SERVER" === #
+
+# === ask user if want to backup === #
+while bool(while_flag):
+    print("サーバーをバックアップしますか？(Y/N)")
+    if bool(error_flag) == False:
+        backupYN1 = input()
+        if backupYN1 == "Y" or backupYN1 == "y":
+            print("バックアップを開始します...")
+            backup_flag = True
+            while_flag = False
+        elif backupYN1 == "n" or backupYN1 == "N":
+            print("バックアップしないでサーバーを起動します...")
+            backup_flag = False
+            while_flag = False
+        else:
+            print("Y/N以外が入力されました。")
+            print()
+# === end "ask user if want to backup" === #
+
+flag_initialize()
+
+if bool(backup_flag) == True:
+    backup_error_flag = False
+    try:
+        shutil.copytree(os.getcwd(), "../Backup" + "/" + str(ut) + "/")
+    except:
+        backup_error_flag = True
+    if bool(backup_error_flag) == False:
+        print("バックアップが完了しました。")
+    elif bool(backup_error_flag) == True:
+        while bool(while_flag):
+            print("バックアップに失敗しました。")
+
+print("ServerHelperを終了します...")
