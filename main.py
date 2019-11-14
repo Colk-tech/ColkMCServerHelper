8c598a2528557a659dabf3b1a922b7f4669aee4c
Colk-tech <mk239312@icloud.com>
2019-11-14 21:48:09 +0900

    Implemented code review from lock at issue #6

--- main.py
+++ main.py
@@ -7,29 +7,41 @@ import ConfigParser
 # refer https://uxmilk.jp/20599 #
 import time
 import shutil
-ut = time.time()
+ut = time.time() #Get unix time to create ID of a backup
 def flag_initialize():
-    global while_flag
-    global error_flag
-    while_flag = True
-    error_flag = False
+    """
+    Initialize all the values of the flags.
+
+    Parameters
+    ----------
+    while_is : bool
+    The flag for while sentences
+    (Every while sentences uses same flag).
+    error_is : bool
+    The flag for errors
+    (Every error checker uses same flag. Therefore, this flag can't be used to find where a error occurs.)
+    """
+    global while_is
+    global error_is
+    while_is = True
+    error_is = False
 flag_initialize()
 Current_Directory: str = os.getcwd()
 # === end initialize === #
 
 # === ask user if want to backup === #
-while bool(while_flag):
+while bool(while_is):
     print("サーバー起動前にバックアップしますか？(Y/N)")
-    if bool(error_flag) == False:
-        backupYN1 = input()
-        if backupYN1 == "Y" or backupYN1 == "y":
+    if bool(error_is) == False:
+        usr_input_backup = input()
+        if usr_input_backup == "Y" or usr_input_backup == "y":
             print("バックアップを開始します...")
-            backup_flag = True
-            while_flag = False
-        elif backupYN1 == "n" or backupYN1 == "N":
+            backup_is = True
+            while_is = False
+        elif usr_input_backup == "n" or usr_input_backup == "N":
             print("バックアップしないでサーバーを起動します...")
-            backup_flag = False
-            while_flag = False
+            backup_is = False
+            while_is = False
         else:
             print("Y/N以外が入力されました。")
             print()
@@ -38,29 +50,29 @@ while bool(while_flag):
 flag_initialize()
 
 # === backup data === #
-if bool(backup_flag) == True:
-    backup_error_flag = False
+if bool(backup_is) == True:
+    backup_error_is = False
     try:
         shutil.copytree(os.getcwd(), "../Backup" + "/" + str(ut) + "/")
     except:
-        backup_error_flag = True
-    if bool(backup_error_flag) == False:
+        backup_error_is = True
+    if bool(backup_error_is) == False:
         print("バックアップが完了しました。")
         print("サーバーを起動します...")
-    elif bool(backup_error_flag) == True:
-        while bool(while_flag):
+    elif bool(backup_error_is) == True:
+        while bool(while_is):
             print("バックアップに失敗しました。")
             print("サーバーを起動しますか？")
-            if bool(error_flag) == False:
-                backupYN1 = input()
-                if backupYN1 == "Y" or backupYN1 == "y":
+            if bool(error_is) == False:
+                usr_input_backup = input()
+                if usr_input_backup == "Y" or usr_input_backup == "y":
                     print("サーバーを起動します。")
-                    backup_flag = True
-                    while_flag = False
-                elif backupYN1 == "n" or backupYN1 == "N":
+                    backup_is = True
+                    while_is = False
+                elif usr_input_backup == "n" or usr_input_backup == "N":
                     print("終了します...")
-                    backup_flag = False
-                    while_flag = False
+                    backup_is = False
+                    while_is = False
                     exit()
                 else:
                     print("Y/N以外が入力されました。")
@@ -75,33 +87,33 @@ os.system("java -Xms4096M -Xmx4096M -jar" + " server.jar " + "-o true")
 # === end "BOOT SERVER" === #
 
 # === ask user if want to backup === #
-while bool(while_flag):
+while bool(while_is):
     print("サーバーをバックアップしますか？(Y/N)")
-    if bool(error_flag) == False:
-        backupYN1 = input()
-        if backupYN1 == "Y" or backupYN1 == "y":
+    if bool(error_is) == False:
+        usr_input_backup = input()
+        if usr_input_backup == "Y" or usr_input_backup == "y":
             print("バックアップを開始します...")
-            backup_flag = True
-            while_flag = False
-        elif backupYN1 == "n" or backupYN1 == "N":
+            backup_is = True
+            while_is = False
+        elif usr_input_backup == "n" or usr_input_backup == "N":
             print("バックアップしないで終了します...")
-            backup_flag = False
-            while_flag = False
+            backup_is = False
+            while_is = False
         else:
             print("Y/N以外が入力されました。")
             print()
 # === end "ask user if want to backup" === #
 
-backup_error_flag = False
+backup_error_is = False
 
-if bool(backup_flag) == True:
+if bool(backup_is) == True:
     try:
         shutil.copytree(os.getcwd(), "../Backup" + "/" + str(ut) + "/")
     except:
-        backup_error_flag = True
-    if bool(backup_error_flag) == False:
+        backup_error_is = True
+    if bool(backup_error_is) == False:
         print("バックアップが完了しました。")
-    elif bool(backup_error_flag) == True:
+    elif bool(backup_error_is) == True:
         print("バックアップに失敗しました。")
         print("終了後、手動でバックアップすることをおすすめします。")
 
